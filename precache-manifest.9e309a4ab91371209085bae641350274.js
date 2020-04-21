ee9f42b7823c3deba45007763030e1a7d7f1550d
Abhijith K Namboothiri <48006342+abhijithknamboothiri@users.noreply.github.com>
2020-04-21 19:41:56 +0530

    Updates

---
+++ precache-manifest.9e309a4ab91371209085bae641350274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7e310cbd8406ea5e76936a7a7a7680",
+    "url": "/my-application/index.html"
+  },
+  {
+    "revision": "edaaea1fb402f11e74cc",
+    "url": "/my-application/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "f3f0dca087c04eb459b5",
+    "url": "/my-application/static/css/main.cb018942.chunk.css"
+  },
+  {
+    "revision": "edaaea1fb402f11e74cc",
+    "url": "/my-application/static/js/2.632eeca7.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-application/static/js/2.632eeca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f0dca087c04eb459b5",
+    "url": "/my-application/static/js/main.71cdbaf5.chunk.js"
+  },
+  {
+    "revision": "386bffd3d81afae8ee75",
+    "url": "/my-application/static/js/runtime-main.52ebab17.js"
+  }
+]);
